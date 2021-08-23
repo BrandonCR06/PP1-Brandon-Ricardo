f81b2f3ecbbe2acd066304b5c0d743c215265efd
BrandonCR06 <rjbrandon0@gmail.com>
2021-08-23 13:55:29 -0600

    Menu, infoAulas

--- main.c
+++ main.c
@@ -1,42 +1,86 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int menu_operativo();
-int menu_general();
+#define TRUE 1
+#define FALSE 0
 
-int main()
-{
-    int opcion;
+//Prototipos de las funciones
+int menuPrincipal();
+int menuOperativo();
+int menuGeneral();
 
-    do
-    {
+//Funciones generales
+char * getString();
+
+//Funciones del submenu operativo
+void infoAulas();
+
+
+
+int main(){
+        menuPrincipal();
+        return 0;
+}
+
+
+int menuPrincipal() {
+        
+        int opcion;
+        char temp[10];
+        
+        do
+        {
         printf( "\n   -==[ Gestion de aulas ]==-");
         printf( "\n   1. Opciones Operativas");
         printf( "\n   2. Opciones Generales");
         printf( "\n   3. Salir");
         printf( "\n\n   Introduzca opciocn (1-3): ");
 
-        scanf( "%d", &opcion );
-
+        fgets(temp,10,stdin);
+        opcion = atoi(temp);
 
         switch ( opcion )
         {
-            case 1: menu_operativo();
-                    break;
-            case 2: menu_general();
-                    break;
+            case 1: 
+                menuOperativo();
+                break;
+
+            case 2: 
+                menuGeneral();
+                break;
+
          }
 
-    } while ( opcion != 3 );
-
+    } while (opcion != 3);
     return 0;
 }
 
 
 
+char * getString(){
+     char letra = ' ';     
+     int i = 0; 
+     char * str = malloc(sizeof(char));
+     while(letra!=10){
+          scanf("%c",&letra);
+          str[i] = letra;
+          i++;
+          str = realloc(str,sizeof(char)*i);          
+     };
+     return str;
+}
 
-int menu_operativo() {
+
+
+
+
+
+
+int menuOperativo() {
     
     int opcion;
+    char temp[10];
 
     do
     {
@@ -51,13 +95,16 @@ int menu_operativo() {
         printf( "\n   8. Salir");
         printf( "\n\n   Introduzca opcion (1-8): ");
 
-        scanf( "%d", &opcion );
+        fgets(temp,10,stdin);
+        opcion = atoi(temp);
 
 
         switch ( opcion )
         {
             case 1: 
+                    infoAulas();
                     break;
+                
             case 2: 
                     break;
             case 3: 
@@ -78,9 +125,54 @@ int menu_operativo() {
 }
 
 
-int menu_general() {
+
+void infoAulas() {
+
+        FILE *archivo; 	
+ 	char caracteres[100];
+        char ruta[50];
+
+        printf("Ingrese la ruta del archivo: ");
+        scanf("%s",ruta);
+
+ 	archivo = fopen(ruta,"r");
+ 	
+ 	if (archivo == NULL)
+ 		exit(1);
+ 	else
+        {
+ 	    printf("\nEl contenido del archivo de prueba es \n\n");
+ 	    while (feof(archivo) == 0) {
+                     
+                     int t = 0;
+                     //Se obtiene un renglon
+                     fgets(caracteres,100,archivo);
+                    
+                     //Se separa
+                     char *token = strtok(caracteres, ",");
+                     while(token != NULL){
+                             if (t = 0) {
+                                printf("Aula: %s ", token);
+                                token = strtok(NULL, "");
+                                t+=1;
+                             }
+                             printf("Cupos: %s ", token);
+                             token = strtok(NULL, "");
+                        }
+
+ 		    //printf("%s",caracteres);
+ 	    }
+    }       
+    fclose(archivo);
+
+}
+
+
+
+int menuGeneral() {
     
     int opcion;
+    char temp[10];
 
     do
     {
@@ -91,7 +183,8 @@ int menu_general() {
         printf( "\n   4. Salir");
         printf( "\n\n   Introduzca opciocn (1-4): ");
 
-        scanf( "%d", &opcion );
+        fgets(temp,10,stdin);
+        opcion = atoi(temp);
 
 
         switch ( opcion )
@@ -106,5 +199,6 @@ int menu_general() {
 
     } while ( opcion != 4 );
     return 0;
-    
+
+
 }
