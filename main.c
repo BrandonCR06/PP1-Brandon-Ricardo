10487b92295c80a107865a74504cfd2fd2498b54
BrandonCR06 <rjbrandon0@gmail.com>
2021-08-23 22:48:07 -0600

    Estructuras para infoProfesores, infoCursor, infoCursosPorPeriodo

--- main.c
+++ main.c
@@ -15,7 +15,9 @@ char * getString();
 
 //Funciones del submenu operativo
 void infoAulas();
-
+void infoProfesores();
+void infoCursos();
+void infoCursosPorPeriodo();
 
 
 int main(){
@@ -31,11 +33,11 @@ int menuPrincipal() {
         
         do
         {
-        printf( "\n   -==[ Gestion de aulas ]==-");
-        printf( "\n   1. Opciones Operativas");
-        printf( "\n   2. Opciones Generales");
-        printf( "\n   3. Salir");
-        printf( "\n\n   Introduzca opciocn (1-3): ");
+        printf( "\n  -==[ Gestion de aulas ]==-");
+        printf( "\n1. Opciones Operativas");
+        printf( "\n2. Opciones Generales");
+        printf( "\n3. Salir");
+        printf( "\n\nIntroduzca opciocn (1-3): ");
 
         fgets(temp,10,stdin);
         opcion = atoi(temp);
@@ -84,16 +86,16 @@ int menuOperativo() {
 
     do
     {
-        printf( "\n   -==[ Menu Operativo ]==-");
-        printf( "\n   1. Información Aulas");
-        printf( "\n   2. Información de Profesores");
-        printf( "\n   3. Información de Cursos");
-        printf( "\n   4. Cursos por Período");
-        printf( "\n   5. Reservación de aula");
-        printf( "\n   6. Cancelar Reservación");
-        printf( "\n   7. Estadísticas");
-        printf( "\n   8. Salir");
-        printf( "\n\n   Introduzca opcion (1-8): ");
+        printf( "\n  -==[ Menu Operativo ]==-");
+        printf( "\n1. Información Aulas");
+        printf( "\n2. Información de Profesores");
+        printf( "\n3. Información de Cursos");
+        printf( "\n4. Cursos por Período");
+        printf( "\n5. Reservación de aula");
+        printf( "\n6. Cancelar Reservación");
+        printf( "\n7. Estadísticas");
+        printf( "\n8. Salir");
+        printf( "\n\nIntroduzca opcion (1-8): ");
 
         fgets(temp,10,stdin);
         opcion = atoi(temp);
@@ -106,10 +108,13 @@ int menuOperativo() {
                     break;
                 
             case 2: 
+                    infoProfesores();
                     break;
             case 3: 
+                    infoCursos();
                     break;
             case 4: 
+                    infoCursosPorPeriodo();
                     break;
             case 5: 
                     break;
@@ -168,6 +173,127 @@ void infoAulas() {
 }
 
 
+void infoProfesores() {
+
+    int opcion;
+    char temp[10];
+
+    do
+    {
+        printf( "\n  -==[ Profesores ]==-");
+        printf( "\n1. Incluir profesor");
+        printf( "\n2. Listar profesores");
+        printf( "\n3. Borrar todo");
+        printf( "\n4. Salir");
+        printf( "\n\nIntroduzca opciocn (1-4): ");
+
+        fgets(temp,10,stdin);
+        opcion = atoi(temp);
+
+
+        switch ( opcion )
+        {
+            case 1: 
+                    printf("Ingrese la cedula del profesor: ");
+                    char * cedula = getString();
+                    printf("Ingrese el nombre del profesor: ");
+                    char * nombre = getString();
+                    //Guardar en la base de datos
+                    break;
+            case 2: 
+                    //Select * from profesores;
+                    break;
+            case 3: 
+                   //Delete From profesores;
+                    break;
+         }
+
+    } while ( opcion != 4 );
+
+}
+
+void infoCursos() {
+
+        //El sistema debe permitir listar los cursos; la información será código de carrera, código de curso y 
+        //nombre. La unicidad la dará el código de curso. El sistema debe tener precargados los cursos en la 
+        //base de datos.
+
+        //Select * from cursos;
+
+}
+
+
+
+void infoCursosPorPeriodo() {
+
+    int opcion;
+    char temp[10];
+
+    do
+    {
+        printf( "\n  -==[ Cursos por periodo ]==-");
+        printf( "\n1. Incluir ");
+        printf( "\n2. Listar");
+        printf( "\n3. Eliminar");
+        printf( "\n4. Salir");
+        printf( "\n\nIntroduzca opciocn (1-4): ");
+
+        fgets(temp,10,stdin);
+        opcion = atoi(temp);
+
+        char *codigoCurso;
+        char *anno;
+        char *p;
+        char *grupo;
+        char *c;
+        int year;
+        int periodo;
+
+        switch ( opcion )
+        {
+            case 1: 
+                    //la información será de código de curso, año, período (1 o 2), 
+                        //grupo, profesor y cantidad de estudiantes.
+                    printf("\nIngrese el codigo del curso: ");
+                    codigoCurso = getString();
+                    printf("\nIngrese el año: ");
+                    anno = getString();
+                    year = atoi(anno);
+                    printf("\nIngrese el periodio ( 1 o 2): ");
+                    p = getString();
+                    periodo = atoi(p);
+                    printf("\nIngrese el nombre del grupo:");
+                    grupo = getString();
+                    printf("\nIngrese la cantidad de estudiantes: ");
+                    c = getString();
+                    int cantEstudiantes = atoi(c);
+                    //Guardar en la base de datos
+                    break;
+            case 2: 
+                    //Select * from cursosPorPeriodo
+                    break;
+            case 3: 
+                   //Borrar
+                   printf("\nIngrese el codigo del curso que desea borrar: ");
+                   codigoCurso = getString();
+                   printf("\nIngrese el año: ");
+                   anno = getString();
+                   year = atoi(anno);
+                   printf("\nIngrese el periodio ( 1 o 2): ");
+                   p = getString();
+                   periodo = atoi(p);
+                   printf("\nIngrese el nombre del grupo:");
+                   grupo = getString();
+                   //delect from cursosPorPeriodo where bla bla bla
+                   break;
+         }
+
+    } while ( opcion != 4 );
+
+}
+
+
+
 
 int menuGeneral() {
     
@@ -176,12 +302,12 @@ int menuGeneral() {
 
     do
     {
-        printf( "\n   -==[ Menu General ]==-");
-        printf( "\n   1. Consulta por dia");
-        printf( "\n   2. Consulta por aula");
-        printf( "\n   3. Consulta por curso");
-        printf( "\n   4. Salir");
-        printf( "\n\n   Introduzca opciocn (1-4): ");
+        printf( "\n  -==[ Menu General ]==-");
+        printf( "\n1. Consulta por dia");
+        printf( "\n2. Consulta por aula");
+        printf( "\n3. Consulta por curso");
+        printf( "\n4. Salir");
+        printf( "\n\nIntroduzca opciocn (1-4): ");
 
         fgets(temp,10,stdin);
         opcion = atoi(temp);
